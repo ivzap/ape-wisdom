910c3d2cd83f817a09cf0950964162b02d1cd977
Ivan and Evan <64557487+ivzap@users.noreply.github.com>
2021-08-11 13:42:49 -0500

    Update ape_main.py

--- ape_main.py
+++ ape_main.py
@@ -1,14 +1,14 @@
-import datetime as dt 
+import datetime as dt
 import pandas as pd
 import requests
 import os
 
 
 #Description: Gets number of pages in pagination
-def get_ape_wisdom_pages(subreddit: str) -> int:
-	url = 'https://apewisdom.io/api/v1.0/filter/' + subreddit + '/'
-	pages = range(1, requests.get(url).json()['pages'] + 1)
-	return pages
+def get_ape_wisdom_pages(subreddit):
+    url = 'https://apewisdom.io/api/v1.0/filter/' + subreddit + '/'
+    pages = range(1, requests.get(url).json()['pages'] + 1)
+    return pages
 
 
 #Description: Gets the data from pagination
@@ -16,8 +16,10 @@ def get_ape_wisdom(subreddit, pages):
     current = dt.datetime.strftime(dt.datetime.now(), format='%Y-%m-%d %H:%M:%S')
     currentDT = dt.datetime.strptime(current,'%Y-%m-%d %H:%M:%S')
     wisdom = pd.DataFrame()
-    for page in pages:  
+    for page in pages:
+
         url = 'https://apewisdom.io/api/v1.0/filter/' + subreddit + '/page/' + str(page)
+
         # request data
         pagination_data = requests.get(url).json()
         tempData = pd.DataFrame(pagination_data['results'])
@@ -34,27 +36,28 @@ def get_ape_wisdom(subreddit, pages):
 
 
 # Description: writes wisdom to current day csv
-def wisdom_to_csv(wisdom: pd.DataFrame, write_dateDT: dt.datetime):
-	cwd = os.getcwd()
-	# Check if we need to write a header to our csv when appending/writing
-	try:
-		# means read was succesful and we already listed the headers in the csv
-		with open(cwd + '/wisdom/' + 'ape_wisdom_' + str(write_dateDT.date()) + '.csv', mode='r') as f:
-		    pass
-		wisdom.to_csv(cwd + '/wisdom/' +  'ape_wisdom_' + str(write_dateDT.date()) + '.csv', mode='a', index=False, header=False)
-	except:
-		# means file not found and this is the first write so header is needed
-		wisdom.to_csv('ape_wisdom_' + str(write_dateDT.date()) + '.csv', mode='w', index=False)
+def wisdom_to_csv(wisdom, write_dateDT):
+    #path = '/home/interns/ape_wisdom/ape-wisdom/wisdom/'
+    path = '/mnt/ape_wisdom_windows_smount/'
+    # Check if we need to write a header to our csv when appending/writing
+    try:
+        # means read was succesful and we already listed the headers in the csv
+        with open(path + 'ape_wisdom_' + str(write_dateDT.date()) + '.csv', mode='r') as f:
+            pass
+        wisdom.to_csv(path + 'ape_wisdom_' + str(write_dateDT.date()) + '.csv', mode='a', index=False, header=False)
+    except:
+        # means file not found and this is the first write so header is needed
+        wisdom.to_csv(path + 'ape_wisdom_' + str(write_dateDT.date()) + '.csv', mode='w', index=False)
 
-	
+
 if __name__ == "__main__":
-	# get subreddit and pages
-	subreddit = 'wallstreetbets'
-	pages = get_ape_wisdom_pages(subreddit)
-	
-	# get new data
-	new_wisdom, write_dateDT = get_ape_wisdom(subreddit, pages)
-	new_wisdom = new_wisdom[['timestamp','ticker', 'name', 'rank', 'mentions','upvotes','rank_24h_ago', 'mentions_24h_ago']]
-	
-	# append/write new wisdom to csv
-	wisdom_to_csv(new_wisdom, write_dateDT)
+    # get subreddit and pages  
+    subreddit = 'wallstreetbets'
+    pages = get_ape_wisdom_pages(subreddit)
+    
+    # get new data
+    new_wisdom, write_dateDT = get_ape_wisdom(subreddit, pages)
+    new_wisdom = new_wisdom[['timestamp','ticker', 'name', 'rank', 'mentions','upvotes','rank_24h_ago', 'mentions_24h_ago']]
+    
+    # append/write new wisdom to csv
+    wisdom_to_csv(new_wisdom, write_dateDT)                                             
