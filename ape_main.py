c8b38d8adb86000287746f98e933ca0f3d5fe9df
Ivan and Evan <64557487+ivzap@users.noreply.github.com>
2021-08-11 15:10:01 -0500

    Update ape_main.py

--- ape_main.py
+++ ape_main.py
@@ -38,15 +38,16 @@ def get_ape_wisdom(subreddit, pages):
 # Description: writes wisdom to current day csv
 def wisdom_to_csv(wisdom, write_dateDT):
     share_mnt_path = '/mnt/ape_wisdom_windows_smount/'
+    file_name = 'ape_wisdom_' + str(write_dateDT.date()) + '.csv'
     # Check if we need to write a header to our csv when appending/writing
     try:
         # means read was succesful and we already listed the headers in the csv
-        with open(share_mnt_path + 'ape_wisdom_' + str(write_dateDT.date()) + '.csv', mode='r') as f:
+        with open(share_mnt_path + file_name, mode='r') as f:
             pass
-        wisdom.to_csv(share_mnt_path + 'ape_wisdom_' + str(write_dateDT.date()) + '.csv', mode='a', index=False, header=False)
+        wisdom.to_csv(share_mnt_path + file_name, mode='a', index=False, header=False)
     except:
         # means file not found and this is the first write so header is needed
-        wisdom.to_csv(share_mnt_path + 'ape_wisdom_' + str(write_dateDT.date()) + '.csv', mode='w', index=False)
+        wisdom.to_csv(share_mnt_path + file_name, mode='w', index=False)
 
 
 if __name__ == "__main__":
